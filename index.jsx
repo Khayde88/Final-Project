b1a60c123872fb07bf62abe52532076490445d74
Khayde88 <144306042+Khayde88@users.noreply.github.com>
2024-04-22 21:20:49 -0500

    Initial code files upload for final project

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import "./simple.css"
+
+<div
+  className="card"
+  style={{width: "24rem", height: "36rem", border: "6px solid", borderRadius: 15}}
+>
+  <div className="card-body">hello</div>
+</div>
