d58e2857915f8f37fa9c94e3efe3fc2ea1d5e961
Noah K <noah-knosp@uiowa.edu>
2024-04-23 21:40:17 -0500

    FrontEndFolder Created
    Main.py Created

--- index.jsx
+++
@@ -1,8 +0,0 @@
-import "./simple.css"
-
-<div
-  className="card"
-  style={{width: "24rem", height: "36rem", border: "6px solid", borderRadius: 15}}
->
-  <div className="card-body">hello</div>
-</div>
---
+++ main.py
@@ -0,0 +1,26 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+
+app = FastAPI()
+
+# Enable CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000"],  # Adjust as needed for your frontend URL
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_index():
+    return FileResponse("./Home.html")
+
+app.mount("/", StaticFiles(directory="Frontend"), name="static")
+
+# Start the FastAPI application
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
