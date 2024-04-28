6537b635fdbd015473056a239e782a05b589ccd5
Noah K <noah-knosp@uiowa.edu>
2024-04-28 01:38:49 -0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb+srv://canderson32:Kotaikanaxai_88@cluster0.dswl3pn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mydb = myclient["grocery_db"]
+mycol = mydb["SavedList"]
+
+mydict = {
+  "date": "1/15/2023",
+  "stores": ["Target", "Whole Foods Market", "Fareway"],
+  "location": "Des Moines, Iowa"
+}
+
+
+x = mycol.insert_one(mydict)
